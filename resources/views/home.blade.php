82efe26b82eeb02f3aaecebf3f044b13bc81a51b
Alhamdanaaa <aalhamdana@gmail.com>
2024-02-21 13:13:37 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Halaman POS</title>
+
+    </head>
+    <body>
+        <h1>Selamat Datang Di Website Point of Sales</h1>
+    </body>
+</html>
