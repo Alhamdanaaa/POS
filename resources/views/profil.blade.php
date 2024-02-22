f0499e64e5b1c66f09db0c9459bf1f26647af92f
Alhamdanaaa <aalhamdana@gmail.com>
2024-02-22 20:59:35 +0700

    Penambahan halaman user

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+    <h1>User {{ $user }}</h1>
+    <h1>Hello, {{ $nama }}</h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,17 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-// Route::prefix('admin')->group(function () {
-//     Route::get('/user', [UserController::class, 'index']);
-//     Route::get('/post', [PostController::class, 'index']);
-//     Route::get('/event', [EventController::class, 'index']);
-//     });
+// Halaman Home
+Route::get('/', [HomeController::class,'index']);
+
+// Halaman Products
 Route::prefix('category')->group(function(){
     Route::get('/food-beverage',[ProductsController::class, 'foodBeverage']);
     Route::get('/beauty-health',[ProductsController::class, 'beautyHealth']);
     Route::get('/home-care',[ProductsController::class, 'homeCare']);
     Route::get('/baby-kid',[ProductsController::class, 'babyKid']);
 });
+
+// Halaman User
+Route::get('/user/{id}/name/{name}', [UserController::class, 'index']);
